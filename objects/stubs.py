fc7af3a179c7cddd96dd9cd189341dd6867e1973
Debianov <regdeb@mail.ru>
2023-01-09 22:46:11 +0800

    Добавлены новые объекты-фрагменты текста сообщений.

--- objects/stubs.py
+++ objects/stubs.py
@@ -1,15 +1,52 @@
-class ChannelMentionText: 
+from typing import Final
 
-	def __init__(self, text: str) -> str:
-		if text.startswith("#"):
-			return text
-		else:
+class Text:
+
+	def __init__(self, text: str) -> None:
+		self.text = text
+		self.processText()
+		self.checkText()
+
+	def __repr__(self):
+		return self.getText()
+
+	def getText(self) -> str:
+		return self.text
+
+	def processText(self) -> None:
+		pass
+
+class StrOrIntText(Text):
+
+	def checkText(self) -> None:
+		if not self.text.isprintable():
 			raise Exception # TODO
 
-class StrOrIntText: #! якорь
+class IntText(Text):
 
-	def __init__(self, text: str) -> str:
-		if text.isalnum():
-			return text
-		else:
+	def checkText(self) -> None:
+		if not self.text.isdigit():
+			raise Exception()
+
+class MentionText(Text):
+	
+	LEFT_BRACKET: Final = "<"
+	RIGHT_BRACKET: Final = ">"
+
+	def checkText(self) -> None:
+		if not self.text.startswith(self.INDICATOR):
 			raise Exception # TODO
+		if not self.text[1:].isdigit():
+			raise Exception
+
+	def processText(self) -> None:
+		self.text = self.text.removeprefix(self.LEFT_BRACKET)
+		self.text = self.text.removesuffix(self.RIGHT_BRACKET)
+
+class ChannelMentionText(MentionText): 
+
+	INDICATOR: Final = "#"
+
+class UserMentionText(MentionText):
+
+	INDICATOR: Final = "@"
