2751b4cc177bbcbfbbb650b8436bb587abd72308
Debianov <regdeb@mail.ru>
2023-04-05 20:56:19 +0800

    Добавлена проверка на вызов подкоманд в командах.

--- bot/commands.py
+++ bot/commands.py
@@ -12,7 +12,27 @@ bot = commands.Bot(
 	intents=intents
 )
 
-@bot.group()
+def is_me():
+	def checkSubcommandPresence(ctx) -> bool:
+		# ctx.command.invoke_without_command #! это всё можно было бы сделать одной строкой, если бы данная штука работала.
+		current_content = ctx.message.content
+		current_content = current_content.removeprefix(ctx.prefix)
+		base_content_len = len(current_content)
+		current_main_command_aliases = list(ctx.command.aliases) + [ctx.command.name]
+		for command_alias in current_main_command_aliases:
+			current_content = current_content.removeprefix(command_alias + " ")
+			if len(current_content) != base_content_len:
+				break
+		for command in ctx.command.commands:
+			for alias in list(command.aliases) + [command.name]:
+				if current_content.startswith(alias):
+					return True
+		return False
+		# TODO вывод ошибки.
+	return commands.check(checkSubcommandPresence)
+
+@bot.group(aliases=["logs"])
+@is_me()
 async def log(ctx: commands.Context) -> None:
 	pass
 
