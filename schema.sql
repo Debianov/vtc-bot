b67791851635cb3603dfb1227e334d8878c1994b
idealism44 <129079769+idealism44@users.noreply.github.com>
2023-04-14 00:56:24 +0300

    Добавление схемы базы данных.

---
+++ schema.sql
@@ -0,0 +1,41 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.2
+-- Dumped by pg_dump version 15.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: target; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.target (
+    target text,
+    act text,
+    d_in text,
+    name text,
+    priority integer,
+    output text,
+    other text
+);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
