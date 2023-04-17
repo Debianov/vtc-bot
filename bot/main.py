1e948e3bf6b1808206d4de67f76609928891212b
Debianov <regdeb@mail.ru>
2023-04-18 02:45:43 +0800

    Обновлён механизм инициализации БД.

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,16 @@
 import discord # TODO отсутствуют stubs-ы.
 import logging
-from bot.commands import bot
+from bot.commands import bot, initDB
 
 from typing import Optional, Union, Tuple
 
 def runForPoetry() -> None:
 	with open("dsAPI_secret.sec") as text:
 		bot.run(text.readline())
+
+async def setup_hook() -> None:
+	await initDB()
+bot.setup_hook = setup_hook
 
 if __name__ == "__main__":
 	runForPoetry()
