806c3ee1eaad82c08ecc0f905155f6cfe7822ec1
idealism44 <129079769+idealism44@users.noreply.github.com>
2023-04-18 22:33:12 +0300

    Рефакторинг скриптов для работы со схемой БД.

--- scripts/setupdb.sh
+++ scripts/setupdb.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ ! -f schema.sql ]]; then
-	printf "%s\n" "Can't find schema.sql. You running script from root repo?" >&2 && exit 1
+if [ ! -f schema.sql ]; then
+	echo "Can't find schema.sql. You running script from root repo?" >&2
+	exit 1
 fi
 
-if [[ $# -ne 2 ]]; then
-	printf "%s\n" "Usage: $0 <db_name> <db_user>" >&2 && exit 2
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <db_name> <db_user>" >&2
+	exit 2
 fi
 
 echo "Creating tables..."
-psql -v ON_ERROR_STOP=1 -U $2 -d $1 -f schema.sql 2>&1 >/dev/null || exit 3
+psql --set=ON_ERROR_STOP=1 --username="$2" --dbname="$1" --file=schema.sql --echo-errors 2>&1 >/dev/null || exit 3
 echo "Tables created."
