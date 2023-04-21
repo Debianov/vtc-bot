cd3588352c4c1a7bee171e13dcff769863d054c4
Debianov <regdeb@mail.ru>
2023-04-21 20:15:59 +0800

    Добавлен минимальный тест.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -4,9 +4,17 @@ import discord.ext.test as dpytest
 import bot.commands as user_commands
 
 @pytest.mark.asyncio
-async def test_log(botInit) -> None:
+async def test_bad_subcommand(botInit) -> None:
+	pass
+
+@pytest.mark.asyncio
+async def test_good_log(botInit) -> None:
 	await dpytest.message("sudo log 1 336420570449051649 asd 748764926897553450 -name ghds")
 	assert dpytest.verify().message().content("Prikol")
+
+@pytest.mark.asyncio
+async def test_bad_log(botInit) -> None:
+	pass
 
 # # TODO вычисляем популярные команды и вставляет в тест сюда. Все команды в
 # # одном файле здесь проверять смысла нет, поскольку для методов, которые
