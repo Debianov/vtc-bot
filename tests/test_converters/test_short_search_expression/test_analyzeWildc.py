abae875a72ef8fe1717c929b28947e517652845e
Debianov <regdeb@mail.ru>
2023-11-10 13:55:40 +0800

    Added a `_analyzeWildcard` test

---
+++ tests/test_converters/test_short_search_expression/test_analyzeWildc.py
@@ -0,0 +1,27 @@
+import pytest
+from discord.ext import commands
+
+from bot.converters import ShortSearchExpression
+from bot.exceptions import ShortSearchExpressionNotFound
+from bot.data import UserGroup, ChannelGroup, DiscordGuildObjects
+
+@pytest.mark.parametrize(
+	"wildcard, compare_data_groups",
+	[
+		("*", UserGroup),
+		("*", ChannelGroup),
+	]
+)
+@pytest.mark.asyncio
+async def test_good_analyzeWildcard_with_one_group(
+	bot: commands.Bot,
+	discordContext: commands.Context,
+	wildcard: str,
+	compare_data_groups
+) -> None:
+	a = ShortSearchExpression[compare_data_groups]()
+	a.wildcard = wildcard
+	a.result: List[DiscordGuildObjects] = []
+	a.data_group_instance = compare_data_groups(discordContext)
+	a._analyzeWildcard()
+	assert a.result == list(compare_data_groups(discordContext).extractData())
