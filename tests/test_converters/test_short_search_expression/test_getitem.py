c0663ae870739efcdf66cde80270a6a64ae22196
Debianov <regdeb@mail.ru>
2023-11-10 13:02:52 +0800

    Add a bad tests

--- tests/test_converters/test_short_search_expression/test_getitem.py
+++ tests/test_converters/test_short_search_expression/test_getitem.py
@@ -1,21 +1,20 @@
-from typing import List, Type
+from typing import Type
 
 import pytest
-from discord.ext import commands
 
 from bot.converters import ShortSearchExpression
-from bot.data import ChannelGroup, DiscordObjectsGroup, UserGroup, TargetGroup
-from bot.exceptions import SearchExpressionNotFound
+from bot.data import ChannelGroup, DiscordObjectsGroup, UserGroup
+
 
 @pytest.mark.parametrize(
 	"data_group_class",
 	[UserGroup, ChannelGroup, DiscordObjectsGroup]
 )
-def test_good(data_group_class: DiscordObjectsGroup) -> None:
+def test_good(data_group_class) -> None:
 	a = ShortSearchExpression[data_group_class]
 	assert a.data_group == data_group_class
 
 def test_without_group_passing() -> None:
 	a = ShortSearchExpression[ChannelGroup]
-	a = ShortSearchExpression
-	assert a.data_group == (last_assignment_class := ChannelGroup)
+	b = ShortSearchExpression
+	assert b.data_group == (last_assignment_class := ChannelGroup) # noqa: F841
