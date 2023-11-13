4594eebad9ff722226ae08617f75a4730e0ffd11
Debianov <regdeb@mail.ru>
2023-11-13 12:57:19 +0800

    Add a new test.

--- tests/test_data/test_data_group_analyz.py
+++ tests/test_data/test_data_group_analyz.py
@@ -44,3 +44,24 @@ def test_good_analyze(
 	)
 	instance.analyze()
 	assert instance.relevant_groups == compare_relevenat_groups_instance
+
+@pytest.mark.parametrize(
+	"group_identif, compare_relevant_groups",
+	[
+		("wrwer+fdsf", []),
+		("ch+ss", [ChannelGroup]),
+		("dfwer", [])
+	]
+)
+def test_bad_analyze(
+	discordContext: commands.Context,
+	group_identif: str,
+	compare_relevant_groups
+) -> None:
+	instance = DataGroupAnalyzator(discordContext, group_identif)
+	compare_relevenat_groups_instance = createDiscordObjectsGroupInstance(
+		compare_relevant_groups,
+		discordContext
+	)
+	instance.analyze()
+	assert instance.relevant_groups == compare_relevenat_groups_instance
