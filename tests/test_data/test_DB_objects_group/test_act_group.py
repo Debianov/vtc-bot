be8dd3ec99606efd0dce9b5a6cd59f1526503555
Debianov <regdeb@mail.ru>
2023-11-24 19:17:51 +0800

    New tests for `DBObjectsGroup`

---
+++ tests/test_data/test_DB_objects_group/test_act_group.py
@@ -0,0 +1,14 @@
+from typing import Type
+
+import pytest
+from discord.ext import commands
+
+from bot.data import ActGroup
+
+def test_identificator():
+	instance = ActGroup()
+	assert instance.DB_IDENTIFICATOR == "act"
+
+def test_extractData_act_group():
+	instance = ActGroup()
+	assert instance.extractData() == [""]
---
+++ tests/test_data/test_DB_objects_group/test_target_group.py
@@ -0,0 +1,9 @@
+from typing import Type
+
+import pytest
+from discord.ext import commands
+
+from bot.data import ActGroup
+
+def test_init_target_group():
+	
