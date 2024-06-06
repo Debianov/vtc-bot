6f48d1ad0a2f6bd696fcffe8ae6a5c0e1a665ba4
Debianov <regdeb@mail.ru>
2024-06-07 01:08:43 +0800

    Fixes some names.

--- tests/test_commands/bad_cases.py
+++ tests/test_commands/bad_cases.py
@@ -1,7 +1,7 @@
 import discord
 
-from bot.utils import (Case, DelayedExpression, ListPartMessage,
-                       ElemFormater, StringPartMessage, default_format)
+from bot.utils import (Case, DelayedExpression, ListMessagePart,
+                       ElemFormater, StringMessagePart, default_format)
 from typing import Any, Union
 
 
@@ -13,29 +13,29 @@ def getDiscordMemberObject(arg: Any) -> Union[str, Any]:
 format_for_getting_ds_id = ElemFormater(getDiscordMemberObject)
 
 case_without_two_required_params = Case(
-    target=ListPartMessage(
+    target=ListMessagePart(
         format_for_getting_ds_id,
         DelayedExpression('mockLocator.members[0]')
     ),
-    act=StringPartMessage(""),
-    d_in=ListPartMessage(default_format, "")
+    act=StringMessagePart(""),
+    d_in=ListMessagePart(default_format, "")
     )
 
 
 case_without_one_required_params = Case(
-    target=ListPartMessage(
+    target=ListMessagePart(
         format_for_getting_ds_id,
         DelayedExpression('mockLocator.members[0]')
     ),
     act="23",
-    d_in=ListPartMessage(default_format, "")
+    d_in=ListMessagePart(default_format, "")
 )
 
 case_without_explicit_flag = Case(
-    target=ListPartMessage(format_for_getting_ds_id,
+    target=ListMessagePart(format_for_getting_ds_id,
                            DelayedExpression("mockLocator.members[0]")),
     act="54",
-    d_in=ListPartMessage(format_for_getting_ds_id,
+    d_in=ListMessagePart(format_for_getting_ds_id,
                          DelayedExpression("mockLocator.members[1]")),
     flags="barhatniy_tyagi"
 )
--- tests/test_commands/good_cases.py
+++ tests/test_commands/good_cases.py
@@ -1,7 +1,7 @@
 import discord
 
-from bot.utils import (Case, DelayedExpression, ListPartMessage,
-                       DictPartMessage, ElemFormater, default_format)
+from bot.utils import (Case, DelayedExpression, ListMessagePart,
+                       DictMessagePart, ElemFormater, default_format)
 from typing import Any, Union
 
 
@@ -13,35 +13,35 @@ def getDiscordMemberObject(arg: Any) -> Union[str, Any]:
 format_for_getting_ds_id = ElemFormater(getDiscordMemberObject)
 
 default_case = Case(
-    target=ListPartMessage(format_for_getting_ds_id,
+    target=ListMessagePart(format_for_getting_ds_id,
                            DelayedExpression('mockLocator.members[0]')),
     act="23",
-    d_in=ListPartMessage(format_for_getting_ds_id,
+    d_in=ListMessagePart(format_for_getting_ds_id,
                          DelayedExpression('mockLocator.members[1]')),
-    flags=DictPartMessage(default_format, {"-name": "Test",
+    flags=DictMessagePart(default_format, {"-name": "Test",
         "-output": "", "-priority": "-1", "other": ""})
 )
 
 default_case_with_several_users = Case(
-    target=ListPartMessage(format_for_getting_ds_id,
+    target=ListMessagePart(format_for_getting_ds_id,
                            DelayedExpression('mockLocator.members[0]'),
                            DelayedExpression('mockLocator.members[1]')),
     act="23",
-    d_in=ListPartMessage(format_for_getting_ds_id,
+    d_in=ListMessagePart(format_for_getting_ds_id,
                          DelayedExpression('mockLocator.members[2]'),
                          DelayedExpression('mockLocator.members[3]')),
-    flags=DictPartMessage(default_format,
+    flags=DictMessagePart(default_format,
                           {"-name": "Test", "-output": "", "-priority": "-1",
          "other": ""})
 )
 
 default_case_with_other_target_name = Case(
-    target=ListPartMessage(format_for_getting_ds_id,
+    target=ListMessagePart(format_for_getting_ds_id,
                            DelayedExpression('mockLocator.members[0]')),
     act="23",
-    d_in=ListPartMessage(format_for_getting_ds_id,
+    d_in=ListMessagePart(format_for_getting_ds_id,
                          DelayedExpression('mockLocator.members[1]')),
-    flags=DictPartMessage(default_format, {"-name": "Aboba",
+    flags=DictMessagePart(default_format, {"-name": "Aboba",
         "-output": "", "-priority": "-1", "other": ""})
 )
 
